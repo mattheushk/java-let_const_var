372522df9dee7c3f991693604abdc83dd2e6dd73
mattheushk <mattheus.krainski@escola.pr.gov.br>
2025-09-18 11:20:57 -0300

    kskks

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+var nomeCantina = "Cantina da Escola;"
+console.log("Bem-vindo à" + nomeCantina);
+let salgados = 20;
+console.log(" Temos" + "salgados disponíveis.");
+const precoSalgado = 5;
+console.log("Cada salgado custa R$" + precoSalgado);
+salgados = salgados - 5; let totalVendido = 5 * precoSalgado;
+console.log("A cantina vendeu R$" + totalVendido);
+precoSalgado = 6; // 
+if (true){
+    var testeVar = "Sou var"; 
+    let testeLet = "Sou let";
+    console.log(testeVar); // 
+    console.log(testeLet); //
+}
+console.log(testeVar); //
+console.log(testeLet);
