840e6221164de1201f19278ff48aefa3d06c624c
mattheushk <mattheus.krainski@escola.pr.gov.br>
2025-09-18 23:03:00 -0300

    a

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+// Variáveis principais
 var nomeCantina = "Cantina da Escola";
 let salgados = 20;
 const precoSalgado = 5;
 let totalVendido = 0;
 
+// Mostrar informações iniciais no layout
 document.getElementById("nome-cantina").textContent = nomeCantina;
 document.getElementById("quantidade-salgados").textContent =
   "Temos " + salgados + " salgados disponíveis.";
 document.getElementById("preco-salgado").textContent =
   "Cada salgado custa R$" + precoSalgado;
 
+// Função de vender 5 salgados
 document.getElementById("vender").addEventListener("click", function () {
   if (salgados >= 5) {
     salgados -= 5;
     totalVendido += 5 * precoSalgado;
 
+    // Atualizar no HTML
     document.getElementById("quantidade-salgados").textContent =
       "Temos " + salgados + " salgados disponíveis.";
     document.getElementById("total-vendido").textContent =
